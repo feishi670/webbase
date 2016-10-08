bbfd271cef47a6d67be52ecefbcbc3fc7dfd2c51
Administrator <Administrator@MS-20161001ZCFI>
2016-10-08 17:10:32 +0800

    base log4j config

---
+++ src/main/java/com/wendell/util/log/Log4jUtil.java
@@ -0,0 +1,35 @@
+package com.wendell.util.log;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jUtil {
+	public static void main(String[] args) throws Exception {
+		
+		setLog4jProperty( "/log4j/log4j.properties");
+		
+		Logger log = Logger.getLogger(Log4jUtil.class);
+		log.info("info");
+		log.error("error");
+	}
+	public static boolean setLog4jProperty(String path){
+		String currentPath=Log4jUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		String logFile =currentPath+path;
+		try {
+			Properties props = new Properties();
+			FileInputStream in = new FileInputStream(logFile);
+			props.load(in);
+			in.close();
+			PropertyConfigurator.configure(props);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
+		return false;
+	}
+}
