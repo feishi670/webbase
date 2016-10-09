631d433942c61bf7462a5c34699e0a70e26bdf52
Administrator <Administrator@MS-20161001ZCFI>
2016-10-09 21:59:35 +0800

    update streamUtil ，add string util

--- src/main/java/com/wendell/util/StreamUtil.java
+++ src/main/java/com/wendell/util/StreamUtil.java
@@ -1,7 +1,11 @@
 package com.wendell.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.log4j.lf5.util.StreamUtils;
 
 public class StreamUtil {
     public static String inStream2String(InputStream is) throws Exception {  
@@ -13,4 +17,13 @@ public class StreamUtil {
         }  
         return new String(baos.toByteArray());  
     } 
+    public static void main(String[] args) {
+    	
+	}
+    public static void copy(InputStream input,OutputStream output) throws IOException{
+		StreamUtils.copy(input, output);
+    }
+    public static byte[] getBytes(InputStream input) throws IOException{
+    	return StreamUtils.getBytes(input);
+    }
 }
