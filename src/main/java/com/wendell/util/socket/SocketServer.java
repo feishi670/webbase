3e718fd693b11928f0306d76abd7becdeceba443
Administrator <Administrator@MS-20161001ZCFI>
2016-10-11 00:09:25 +0800

    update socket server to write to target client.

--- src/main/java/com/wendell/util/socket/SocketServer.java
+++ src/main/java/com/wendell/util/socket/SocketServer.java
@@ -6,13 +6,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SocketServer {
 	private ServerSocket server;
 	private int port;
-	private Socket socket;
-	private PrintWriter out;
+	private List<Socket> clients=new ArrayList<Socket>();
 
 	public SocketServer(int port) {
 		this.port=port;
@@ -54,23 +56,35 @@ public class SocketServer {
 		if(client==null){
 			return false;
 		}
+		clients.add(client);
 		onAccept(client);
 		new Thread(){
 			public void run () {
 				try {
 					BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); 
 					while (true) {  
-						String msg = in.readLine();  
-						onMessage(client,msg);
-						if (msg.equals("end"))  
+						try {
+							String msg = in.readLine();  
+							onMessage(client,msg);
+							if (msg.equals("end")){
+								break;  
+							}
+						} catch (SocketException e) {
+							// TODO: handle exception
+							System.out.println(e.getMessage());
 							break;  
+						}
 					}  
 					client.close();  
+					clients.remove(client);
 				} catch (Exception e) {
 					e.printStackTrace();
+					System.out.println(e.getClass().getName());
+					
 				}
 			};
 		}.start();;
+		
 			return true;
 	}
 	private void onAccept(Socket client) {
@@ -98,28 +112,28 @@ public class SocketServer {
 		System.out.println(msg);
 	}
 	
-	public void write(String msg) throws IOException{
-		System.out.println("send msg:"+msg);
+	public void writeToAll(String msg) throws IOException{
+			System.out.println("send msg:"+msg+"  to client;client num:"+clients.size());
+			for(Socket socket:clients){
+				write(socket,msg);
 
-	      this.out =getWriter(); new PrintWriter(socket.getOutputStream());  
-          out.println(msg);  
-          out.flush();  
+			}
+	}
+	public void write(Socket socket,String msg) throws IOException{
+//		socket.getOutputStream().write(msg.getBytes());
+//		socket.getOutputStream().flush();
+		PrintWriter out = new PrintWriter(socket.getOutputStream());  
+		out.println(msg);  
+		out.flush();  
 	}
 	
-	private PrintWriter getWriter() throws IOException {
-		// TODO Auto-generated method stub
-		if( this.out!=null){
-			return this.out;
-		}
-		return     this.out =new PrintWriter(socket.getOutputStream()); 
-	}
 	public static void main(String[] args) throws IOException {
 		SocketServer server=new SocketServer(9096);
 		server.startListener();
 		Scanner reader=new Scanner(System.in);
 		System.out.println("server start");
 		while(reader.hasNext()){
-			server.write(reader.nextLine());
+			server.writeToAll(reader.nextLine());
 		}
 		System.out.println("read all");
 	}
